f838e04c8fa96f9983e4b751ff3cb838f9d9970a
Fatilay <fatmatilay@gmail.com>
2025-01-22 16:53:28 +0300

    first commit

---
+++ 1_hazirlik.py
@@ -0,0 +1,24 @@
+import shutil
+import os
+
+folders = ["../veri_setleri/orijinal_halleri/train", 
+           "../veri_setleri/orijinal_halleri/valid", 
+           "../veri_setleri/orijinal_halleri/test"]
+
+# print("folders: ", folders)
+
+combined_folder = "./veri_setleri/birlestirilmis_hali"  # Tüm verilerin birleştirileceği klasör
+
+# Hedef klasör oluştur
+os.makedirs(combined_folder, exist_ok=True)
+
+# Mevcut verileri birleştir
+for folder in folders:
+    for class_name in os.listdir(folder):
+        class_path = os.path.join(folder, class_name)
+        combined_class_path = os.path.join(combined_folder, class_name)
+        os.makedirs(combined_class_path, exist_ok=True)
+        for img in os.listdir(class_path):
+            shutil.copy(os.path.join(class_path, img), combined_class_path)
+
+print("Veriler başarıyla birleştirildi!")
---
+++ 2_ayirma.py
@@ -0,0 +1,17 @@
+# combined_folder = "./veri_setleri_2/birlestirilmis_hali"  # yeni birlestirdigimiz veri setinin dosyadaki kisayolu
+
+import splitfolders  # splitfolders kütüphanesini içe aktarın
+
+# Birleştirilen veri kümesinin yolu
+input_folder = "./veri_setleri/birlestirilmis_hali"
+output_folder = "./veri_setleri/tekrardan_bolunmus_hali"
+
+# %70 Eğitim, %10 Doğrulama, %20 Test oranlarına göre bölme işlemi
+splitfolders.ratio(
+    input_folder, 
+    output=output_folder, 
+    seed=42,  # Rastgelelik için sabit bir değer
+    ratio=(0.7, 0.1, 0.2)  # Eğitim, Doğrulama, Test oranları
+)
+
+print("Veri başarıyla %70-%10-%20 oranlarında bölündü!")
